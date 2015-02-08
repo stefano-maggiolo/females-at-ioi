fe80d1a8b785af478187fa106043930903866897
Stefano Maggiolo <s.maggiolo@gmail.com>
2015-02-08 19:13:21 +0000

    Initial

---
+++ extract_names.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import io
+from glob import glob
+from bs4 import BeautifulSoup
+
+delegations = [x.strip().split(',') for x in open("data/delegations")]
+names = []
+for d in delegations:
+    print d
+    page = BeautifulSoup(open("data/%s.%s" % (d[0], d[1])).read())
+    sections = page.find_all(class_='basicname')
+    for s in sections:
+        s_name = s.string
+        for name in  s.next_sibling.find_all(class_="name"):
+            img = name.previous_sibling
+            img_src = ''
+            if img is not None and img.name == 'img':
+                img_src = img.attrs['src']
+            names.append((d[0], d[1], s_name, name.string, img_src))
+
+with io.open("data/names", "w", encoding="utf-8") as f:
+    for n in names:
+        f.write("%s,%s,%s,%s,%s\n" % n)
+
