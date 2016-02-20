205d9a5b1cd7f01f910a99ead57a57cd2cbb772e
Stefano Maggiolo <s.maggiolo@gmail.com>
2016-02-20 13:52:03 +0000

    Extract also stats ids

--- extract_names.py
+++ extract_names.py
@@ -12,14 +12,15 @@ for d in delegations:
     sections = page.find_all(class_='basicname')
     for s in sections:
         s_name = s.string
-        for name in  s.next_sibling.find_all(class_="name"):
+        for name in s.next_sibling.find_all(class_="name"):
             img = name.previous_sibling
             img_src = ''
             if img is not None and img.name == 'img':
                 img_src = img.attrs['src']
-            names.append((d[0], d[1], s_name, name.string, img_src))
+            id_ = name.parent.attrs['href'].replace('people/', '')
+            names.append((d[0], d[1], s_name, name.string, img_src, id_))
 
 with io.open("data/names", "w", encoding="utf-8") as f:
     for n in names:
-        f.write("%s,%s,%s,%s,%s\n" % n)
+        f.write("%s,%s,%s,%s,%s,%s\n" % n)
 
